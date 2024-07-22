c40d5da350206bbcc801901456cc8d06f8ddd3be
love olubummo <olubummolove3@gmail.com>
2024-07-22 18:55:13 +0100

    update highlights component

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,8 +1,32 @@
 // import React from 'react'
 
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap";
+import {rightImg, watchImg} from '../utils'
+
 const Highlights = () => {
+  // animations
+  useGSAP(()=>{
+    gsap.to('#title', {opacity: 1, y: 0});
+
+    gsap.to('.link', {opacity: 1, y: 0, duration: 1, stagger: 0.25})
+  },[])
   return (
-    <div>Highlights</div>
+    <section className="w-screen h-full bg-zinc overflow-hidden common-padding">
+      <div className="screen-max-width">
+        <div className="mb-12 w-full md:flex items-end justify-between ">
+          <h1 id="title"  className=" ">
+            Get the highlights
+          </h1>
+          <div className="flex flex-wrap items-baseline gap-5">
+            <p className="link">Watch the film</p>
+            <img src={watchImg} alt="watch" className="ml-2"/>
+            <p className="link">Watch the event</p>
+            <img src={rightImg} alt="right" className="ml-2"/>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
