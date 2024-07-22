b095def79f452b4816346008e2a2beded8453971
love olubummo <olubummolove3@gmail.com>
2024-07-22 18:09:06 +0100

    update hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,42 @@
-// import React from 'react'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import {heroVideo, smallHeroVideo} from '../utils';
+import { useEffect, useState } from "react";
 
 const Hero = () => {
+  // assign video
+  const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo);
+  // function to change  video based on screen size
+  const videoSwap = ()=>{
+    if(window.innerWidth < 760) setVideoSrc(smallHeroVideo);
+    else{
+      setVideoSrc(heroVideo)
+    }
+  }
+  // trigger
+  useEffect(()=>{
+    window.addEventListener('resize', videoSwap);
+
+    return ()=>{
+      window.removeEventListener('resize', videoSwap);
+    }
+  },[])
+  useGSAP(()=>{
+    gsap.to('#hero', {opacity: 1, delay: 1.5})
+  },[])
   return (
-    <div>Hero</div>
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id='hero' className="hero-title">
+          iphone 15 Pro
+        </p>
+        <div className="w-9/12 md:w-10/12">
+          <video className="pointer-events-none" muted autoPlay playsInline={true} key={videoSrc}>
+            <source src={videoSrc} type="video/mp4"/>
+          </video>
+        </div>
+      </div>
+    </section>
   )
 }
 
