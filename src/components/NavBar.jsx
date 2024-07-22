0ea252f73abc9632d8b25f82e99fe665d9ab0f64
love olubummo <olubummolove3@gmail.com>
2024-07-22 17:19:44 +0100

    update navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,18 @@
-import {appleImg, bagImg, searchImg} from '../utils'
+import {appleImg, bagImg, searchImg} from '../utils';
+import { navLists } from '../constant';
 const NavBar = () => {
   return (
-    <header>
-      <nav>
+    <header className='w-full py-5 px-5 sm:py-10 flex justify-between items-center'>
+      <nav className='flex w-full screen-max-width'>
         <img src={appleImg} alt="apple icon" width={14} height={18} />
 
-        <div>
-          {['Phones', 'Macbooks', 'Tablets'].map((device)=><div key={device}>{device}</div>)}
+        <div  className='flex flex-1 justify-center max-sm:hidden'>
+          {navLists.map((device)=><div key={device} 
+          className='px-5 text-sm text-gray hover:text-white cursor-pointer transition-all'
+          >{device}</div>)}
         </div>
         
-        <div>
+        <div className='flex gap-7 items-baseline max-sm:justify-end max-sm:flex-1 '>
           <img src={searchImg} alt="search" width={18} height={18}/>
           <img src={bagImg} alt="bag" width={18} height={18}/>
         </div>
